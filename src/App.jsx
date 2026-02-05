412e3cd7da2234b805dcb9bae2aacfcc550f9024
Andrei Gobres <andreigobres3@gmail.com>
2026-02-05 22:22:29 +0800

    react + tailwindcss project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+   <div>
+      <p className="bg-emerald-500 font-bold text-xl p-4 text-center text-slate-700 font-header">All your files</p>
+   </div>
+  )
+}
+
+export default App
