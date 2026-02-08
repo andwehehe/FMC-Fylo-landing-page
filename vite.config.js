e7b4b8e064454ba12ae5e99a419d8c8ad6d883d1
Andrei Gobres <andreigobres3@gmail.com>
2026-02-08 10:53:29 +0800

    error 404 fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/FMC-Fylo-landing-page',
+  base: '/FMC-Fylo-landing-page/',
 })
